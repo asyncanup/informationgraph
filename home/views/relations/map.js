a012319eceadc10a5335a5a6c517a3bca4fa1fd7
Anup Bishnoi <pixelsallover@gmail.com>
2011-02-21 01:56:40 +0530

    added empty rewrites file, changed relations view to let it include the relation document if asked by include_docs, and added stub README

--- home/views/relations/map.js
+++ home/views/relations/map.js
@@ -1,6 +1,6 @@
 function(doc){
   if (doc.type === "relation"){
-    emit ([doc.subject, "subject"], doc._id);
-    emit ([doc.object, "object"], doc._id);
+    emit ([doc.subject, "subject"], {"_id": doc._id});
+    emit ([doc.object, "object"], {"_id": doc._id});
   }
 };
