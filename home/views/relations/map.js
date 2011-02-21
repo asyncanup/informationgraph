6528da43030d2ffbe5582cc5ed6108a9842b7318
Anup Bishnoi <pixelsallover@gmail.com>
2011-02-21 19:08:11 +0530

    changed the emitted value in relations view

--- home/views/relations/map.js
+++ home/views/relations/map.js
@@ -1,6 +1,8 @@
 function(doc){
   if (doc.type === "relation"){
-    emit ([doc.subject, "subject"], {"_id": doc._id});
-    emit ([doc.object, "object"], {"_id": doc._id});
+    // query the view with include_docs=true to get the relation docs
+    // or just include the docs here if view response speed is a concern.
+    emit ([doc.subject, "subject"], null); 
+    emit ([doc.object, "object"], null);
   }
 };
