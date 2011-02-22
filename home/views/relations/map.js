1e47753792d5227061e30eab28bfde68c570e71b
Anup Bishnoi <pixelsallover@gmail.com>
2011-02-22 13:57:50 +0530

    no change

--- home/views/relations/map.js
+++ home/views/relations/map.js
@@ -1,7 +1,6 @@
 function(doc){
   if (doc.type === "relation"){
-    // query the view with include_docs=true to get the relation docs
-    // or just include the docs here if view response speed is a concern.
+    // think about adding doc.subject.value before doc.subject here
     emit ([doc.subject, "subject"], null); 
     emit ([doc.object, "object"], null);
   }
