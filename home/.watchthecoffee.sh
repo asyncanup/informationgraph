24e2a2fa576675a20073c495ae4ef22358270252
Anup Bishnoi <pixelsallover@gmail.com>
2011-04-25 18:43:47 +0530

    using coffeeapp

--- home/.watchthecoffee.sh
+++
@@ -1,4 +0,0 @@
-cd ~/code/informationgraph/home
-coffee --watch --compile --bare -o _attachments/scripts/ -c coffeescript/app/ &
-coffee --watch --compile --bare -o vendor/ig/_attachments/ -c coffeescript/ig/ &
-coffee --watch --compile --bare -o views/ -c coffeescript/views/ &
--- home/views/relations/map.js
+++
@@ -1,7 +0,0 @@
-(function(doc) {
-  if (doc.type === "relation") {
-    emit([doc.subject, "s"], null);
-    emit([doc.predicate, "p"], null);
-    return emit([doc.object, "o"], null);
-  }
-});
